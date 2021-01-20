e920caeecbfe5a0ae81040fa4673c5f7493501f2
BerryVanSomeren <berryvansomeren@gmail.com>
2021-01-20 15:20:10 +0100

    Due to moving some functionality to cmake generator, the scripts generating the cmake file are now more minimal

--- generate_cmake_file.py
+++ generate_cmake_file.py
@@ -1,8 +1,9 @@
-from cmake_generator import Project, Version, absp
+import logging
+
+from cmake_generator import Project, Version, absp, generate_cmake_file
 
 # ----------------------------------------------------------------
-def get_project_definition() -> Project:
-
+def main():
     target_definition_file_paths = [
         absp( path ) + "/target_definitions.py" for path in [
             "./external/",
@@ -17,10 +18,17 @@ def get_project_definition() -> Project:
         ]
     ]
 
-    return Project(
+    cmake_project_definition = Project(
         project_name                        = "shake",
         version                             = Version( 4, 0 ),
         destination_cmake_lists_file_path   = absp( "./CMakeLists.txt" ),
         build_directory_path                = absp( "./build/" ),
         target_definition_file_paths        = target_definition_file_paths
     )
+
+    generate_cmake_file( cmake_project_definition )
+
+# ----------------------------------------------------------------
+if __name__ == "__main__":
+    logging.basicConfig( level = logging.INFO )
+    main()
